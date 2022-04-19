559895953a943c693ac7d04161cb5686e555041a
yujuava <yju031769@gmail.com>
2022-04-19 21:00:08 +0800

    修改

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+function $id(id){
+    return document.getElementById(id)
+}
 function closeDetail(){
     document.getElementById("detailChoose").style.display="none";
     var closes = document.getElementsByTagName("input");
@@ -5,6 +8,26 @@ function closeDetail(){
         if(closes[i].type=="checkbox")
         {closes[i].checked =false; 
         }
+    }
+}
+var num=4; 
+var timerId;
+countDown();
+    timerId =window.setInterval(countDown,2000)//計時器單位是毫秒
+function countDown(){
+    num--;
+    if(num==3){
+        $id("a").classList.add("opcupy");
+        $id("c").classList.remove("opcupy");
+    }
+    if(num==2){
+        $id("a").classList.remove("opcupy");
+        $id("b").classList.add("opcupy");
+    }
+    if(num==1){
+        $id("b").classList.remove("opcupy");
+        $id("c").classList.add("opcupy");
+        num=4;
     }
 }
 function init(){
