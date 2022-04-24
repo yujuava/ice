f417af3a7af62893b6b7d473b84512066232f962
yujuava <yju031769@gmail.com>
2022-04-25 01:53:27 +0800

    修改

--- js/closeBox.js
+++ js/closeBox.js
@@ -1,10 +1,14 @@
 function $id(id){
     return document.getElementById(id)
 }
+let itemNum = 0;
 function closebox(){
     document.getElementById("detailChoose").style.display="none";
     $id('chooseItems').style.display="block";
-    $id('chooseItems').innerHTML="1";
+    itemNum++;
+    $id('chooseItems').innerHTML=itemNum;
+    console.log("我在哪")
+        
 }
 function init(){
     $id('addShoppingCar').onclick=closebox;
